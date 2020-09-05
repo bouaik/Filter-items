1b1dea91afa2bf4f8b974ad1a9b629d0ec796381
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-05 16:42:25 +0100

    make logic to filter items

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 (function () {
-    const filterBtn = document.querySelectorAll('.filter-btn')
+  const filterBtn = document.querySelectorAll(".filter-btn");
 
-    filterBtn.forEach( (btn) => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault()
-            const value = e.target.dataset.filter
+  filterBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const value = e.target.dataset.filter;
 
-        })
-    })
+      const items = document.querySelectorAll(".store-item");
 
-
-
-
-    
+      items.forEach((item) => {
+        if (value === "all") {
+          item.style.display = "block";
+        } else {
+          if (item.classList.contains(value)) {
+            item.style.display = "block";
+          } else {
+            item.style.display = "none";
+          }
+        }
+      });
+    });
+  });
 })();
